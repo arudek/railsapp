e0bd5093ed33b96de858df0a12c0739da9118f71
Arne Rudek <arne.rudek@helppo.de>
2011-05-31 15:02:54 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = '900b5322636f366677fd1f8cbb6cf388802206c84663430eeaa279d63b3a4f9f8a6202092f3aa71cabc97ce31e247afae67243d921f0227d7edf12c4c03b1049'
---
+++ db/migrate/20110531125918_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.text :bio
+      t.date :age
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
